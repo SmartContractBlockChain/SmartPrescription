a4302f74cd4d582736e10700c9608ccfdd8755b8
Kajetan <drakles1@gmail.com>
2021-03-14 16:55:57 +0000

    small refactoring

--- integration.py
+++ integration.py
@@ -17,12 +17,10 @@ def get_contract(w3, contract_interface, address):
 
 def deploy_contract(w3, contract_interface, from_account, medicineName, patientAddress):
     tx_hash = w3.eth.contract(
-        abi=contract_interface['abi'], bytecode=contract_interface['bin']).constructor(medicineName,
-                                                                                       patientAddress).transact(
-        {'from': from_account})
+        abi=contract_interface['abi'], bytecode=contract_interface['bin']) \
+        .constructor(medicineName, patientAddress).transact({'from': from_account})
 
-    address = w3.eth.getTransactionReceipt(tx_hash)['contractAddress']
-    return address
+    return w3.eth.getTransactionReceipt(tx_hash)['contractAddress']
 
 
 if __name__ == '__main__':
@@ -34,7 +32,7 @@ if __name__ == '__main__':
     contract_interface = compile_source_contract(contract_source_path)
 
     # obtain address of deployed contract
-    address = deploy_contract(w3, contract_interface, w3.eth.accounts[0], 'viagra', w3.eth.accounts[0])
+    address = deploy_contract(w3, contract_interface, w3.eth.accounts[0], 'viagra', w3.eth.accounts[1])
 
     # interact with contract from blockchain
-    print(get_contract(w3, contract_interface, address).functions.medicineName().call())
+    print('Your medicine is: '+str(get_contract(w3, contract_interface, address).functions.medicineName().call()))
