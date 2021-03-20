a6cfad65522618127eb03122efe325faf72f155d
Tim Heller <63578503+tim-hash@users.noreply.github.com>
2021-03-20 19:10:03 +0000

    Merged smart contract and API

--- app.py
+++ app.py
@@ -2,162 +2,185 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
 import random
+from web3 import Web3, HTTPProvider
+from solcx import compile_source
+from helpers import compile_source_contract, get_contract, deploy_contract
+
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)  
+    # Defining global variables
+    blockchain_address = "HTTP://127.0.0.1:7545"
+    w3 = Web3(HTTPProvider(blockchain_address))
+    contract_source_path = "smartContracts/Prescription.sol"
+    contract_interface = compile_source_contract(contract_source_path)
+
+    # create and configure the app
+    app = Flask(__name__)
+    # Set up CORS. Allow '*' for origins.
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+    # after_request decorator to set Access-Control-Allow
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type, Authorization, true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS"
+        )
+        return response
+
+    """
+GET requests for drugs list.
+
+Retrieve drugs list 
+  * name 
+  * availability (optional) 
+
+Authorized users (Doctor and Pharmacist)
+"""
+
+    @app.route("/drugs")
+    def get_drugs_list():
+
+        return jsonify({"success": True,}), 200
+
+    """
+GET requests for drug information.
+
+Retrieve drug information by drug_name
+  * name 
+  * strength
+  * formulation
+  * availability (optional) 
   
-  # Set up CORS. Allow '*' for origins.
-  CORS(app, resources={r"/api/*" : {'origins': '*'}})
+Authorized users (Doctor and Pharmacist)
+"""
 
-  # after_request decorator to set Access-Control-Allow
-  @app.after_request
-  def after_request(response):
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization, true')
-    response.headers.add('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
-    return response
+    @app.route("/drugs/<string:drug_name>")
+    def get_drug_by_name(drug_name):
 
-  '''
-  GET requests for drugs list.
+        return jsonify({"success": True,}), 200
 
-  Retrieve drugs list 
-    * name 
-    * availability (optional) 
+    """
+GET requests for prescription.
 
-  Authorized users (Doctor and Pharmacist)
-  '''
-  @app.route('/drugs')
-  def get_drugs_list():
-
-    return jsonify({
-        'success': True,
-    }),200
-
-  '''
-  GET requests for drug information.
-
-  Retrieve drug information by drug_name
-    * name 
-    * strength
-    * formulation
-    * availability (optional) 
-    
-  Authorized users (Doctor and Pharmacist)
-  '''
-  @app.route('/drugs/<string:drug_name>')
-  def get_drug_by_name(drug_name):
-
-    return jsonify({
-        'success': True,
-    }),200
-
-  '''
-  GET requests for prescription.
-
-  Retrieve prescription details by patient_address
-    * drug name
-    * directions
-    * quantity
-    * signature  (name and address of prescriber)
-    * date of issue 
-    * isUsed (boolean) 
-    
-  Authorized users (Doctor and Pharmacist)
-  '''
-  @app.route('/prescription/<string:patient_address>')
-  def get_prescription(patient_address):
-
-    return jsonify({
-        'success': True,
-    }),200
-
-
-  '''
-  GET requests for prescriber infromation.
-
-  Retrieve prescriber infromation by the signature
-    * prescriber name
-    * address
-    
-  Authorized users (Doctor and Pharmacist)
-  '''
-  @app.route('/prescription/<string:signature>')
-  def get_prescriber(signature):
-
-    return jsonify({
-        'success': True,
-    }),200
-
-  '''
-  POST requests for creating prescription.
-
-  create prescription
-    * prescriber information
-    * patient_address
-    * drug 
-        - name 
-        - directions
-        - quantity
-    * date of issue
-    
-  Authorized user (Doctor)
-  '''
-  @app.route('/prescription', methods=['POST'])
-  def create_prescription():
-
-    return jsonify({
-        'success': True,
-    }),200
+Retrieve prescription details by the contract transaction hash
+  * patient_address
+  * drug name
+  * directions
+  * quantity
+  * signature  (name and address of prescriber)
+  * date of issue 
+  * isUsed (boolean) 
   
-  '''
-  POST requests for redeem prescription.
+Authorized users (Doctor and Pharmacist)
+"""
 
-  Redeem prescription
-    * patient_address
-    * setPharmacist
-    * check isUsed
-        - if true --> reject the request
-        - if false --> 1) accsept the request and 2) set isUsed to true
-        
-  Authorized users (Pharmacist)
+    @app.route("/prescription/<string:tx_hash>")
+    def get_prescription(tx_hash):
+        address = w3.eth.getTransactionReceipt(tx_hash)["contractAddress"]
+        prescription = (
+            get_contract(w3, contract_interface, address)
+            .functions.getPrescription()
+            .call() #.call({'from':web3.eth.accounts[1]}) to select the wallet from which to call the function
+        )
+        return jsonify({"success": True, "Prescription": prescription}), 200
 
-    NOTE: check of the varibale in the request to use wither patient_address or pharmacist_address
-  '''
-  @app.route('/redeem/<string:patient_address>', methods=['POST']) 
-  def redeem_prescription():
+    """
+GET requests for prescriber infromation.
 
-    return jsonify({
-        'success': True,
-    }),200
-        
-  '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
-
-  @app.errorhandler(404)
-  def not_found(error):
-    return jsonify({
-      'success': False,
-      'error': 404,
-      'message': 'resource not found'
-    }),404
-
-  @app.errorhandler(422)
-  def unprocessable(error):
-    return jsonify({
-      'success': False,
-      'error': 422,
-      'message': 'unprocessable'
-    }),422
-
-  @app.errorhandler(400)
-  def bad_request(error):
-    return jsonify({
-      'success': False,
-      'error': 400,
-      'message': 'bad request'
-    }),400
+Retrieve prescriber infromation by the signature
+  * prescriber name
+  * address
   
-  return app
+Authorized users (Doctor and Pharmacist)
+"""
+
+    @app.route("/prescriber/<string:tx_hash>")
+    def get_prescriber(tx_hash):
+        address = w3.eth.getTransactionReceipt(tx_hash)["contractAddress"]
+        creator = (
+            get_contract(w3, contract_interface, address).functions.creator().call()
+        )
+
+        return jsonify({"success": True, "Prescriber": creator}), 200
+
+    """
+POST requests for creating prescription.
+
+create prescription
+  * patient_address
+  * directions
+  * quantity
+  * date
+  * drug 
+      - name 
+      - strength
+      - formulation
+  * date of issue
+  
+Authorized user (Doctor)
+"""
+
+    @app.route("/prescription", methods=["POST"])
+    def create_prescription():
+        request_data = request.get_json()
+        _contractVariables = [
+            request_data["patientAddress"],
+            request_data["directions"],
+            request_data["quantity"],
+            request_data["date"],
+            request_data["drugName"],
+            request_data["drugStrength"],
+            request_data["drugFormulation"],
+        ]
+        address = deploy_contract(
+            w3, contract_interface, w3.eth.accounts[0], _contractVariables
+        )
+        return jsonify({"success": True, "contractAddress": address}), 200
+
+    """
+POST requests for redeem prescription.
+
+Redeem prescription
+  * patient_address
+  * setPharmacist
+  * check isUsed
+      - if true --> reject the request
+      - if false --> 1) accsept the request and 2) set isUsed to true
+      
+Authorized users (Pharmacist)
+
+  NOTE: check of the variable in the request to use wither patient_address or pharmacist_address
+"""
+
+    @app.route("/redeem/<string:tx_hash>", methods=["POST"])
+    def redeem_prescription(tx_hash):
+        address = w3.eth.getTransactionReceipt(tx_hash)["contractAddress"]
+        isUsed = get_contract(w3, contract_interface, address).functions.redeem().call()
+        return jsonify({"success": True, "Prescription used": isUsed}), 200
+
+"""
+Error Handling
+"""
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return (
+            jsonify({"success": False, "error": 404, "message": "resource not found"}),
+            404,
+        )
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return (
+            jsonify({"success": False, "error": 422, "message": "unprocessable"}),
+            422,
+        )
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({"success": False, "error": 400, "message": "bad request"}), 400
+
+    return app
--- smartContracts/Prescription.sol
+++ smartContracts/Prescription.sol
@@ -1,14 +1,12 @@
 pragma solidity >=0.7.0;
 
-
-contract Prescription {
-
+contract smartPrescription {
     // usually prescription is made by doctor, but here we will stick to general term of prescription creator
     // The keyword public automatically generates a function that allows you to access the current value of
     // the state variable from outside of the contract.
     address public creator;
 
-     struct Drug{
+    struct Drug {
         string name;
         string strength;
         string formulation;
@@ -23,6 +21,10 @@ contract Prescription {
         string date;
     }
 
+    Prescription prescription;
+
+    Drug drug;
+
     address patient;
 
     // lets assume pharmacist will be visible as well
@@ -31,28 +33,67 @@ contract Prescription {
     // to indicate wheather prescription was used
     bool public isUsed;
 
-    constructor(address _patient_address, string memory _directions, string memory _quantity, string memory _date, string memory _name, string memory _strength, string memory _formulation) {
-        creator = msg.sender; // TODO: Make a check that the construction of the prescription can only be done by a doctor with a require from a database of doctors address?
-        Drug memory drug = Drug(_name, _strength, _formulation);
-        Prescription memory prescription = Prescription(_patient_address, drug, _directions, _quantity, creator, _date);
-        patient=_patient_address;
-        isUsed=false;
-        
+    constructor(
+        address _patient_address,
+        string memory _directions,
+        string memory _quantity,
+        string memory _date,
+        string memory _name,
+        string memory _strength,
+        string memory _formulation
+    ) {
+        creator = msg.sender;
+        drug = Drug(_name, _strength, _formulation);
+        prescription = Prescription(
+            _patient_address,
+            drug,
+            _directions,
+            _quantity,
+            creator,
+            _date
+        );
+        patient = _patient_address;
+        isUsed = false;
+
         // here we wont initialize address of pharmacist,
         // as it can be only done when patient will be going to use its prescription
     }
 
     function setPharmacist(address _pharmacist) public {
         //Should set pharmacist before patient can retrieve his prescription, done by patient
-        require(msg.sender == patient) //this way no one else than the patient can retrieve the prescription
+        //this way no one else than the patient can retrieve the prescription
         pharmacist = _pharmacist;
     }
-    
 
-    function reedem(address _patient) public{
-        // here we should add more validation to avoid calling this method more than once, anyway to delete the contract once done ?
-        require(msg.sender == pharmacist && isUsed==false);
+    function getPrescription()
+        public
+        view
+        returns (
+            address,
+            string memory,
+            string memory,
+            string memory,
+            string memory,
+            string memory,
+            string memory,
+            bool
+        )
+    {
+        return (
+            prescription.patient_address,
+            prescription.directions,
+            prescription.quantity,
+            prescription.date,
+            drug.name,
+            drug.strength,
+            drug.formulation,
+            isUsed
+        );
+    }
+
+    function redeem() public returns (bool) {
+        require(isUsed == false);
         isUsed = true;
-        //TODO: need to display the prescription here when redeeming so pharmacist can atctually do it 
+        return (isUsed);
     }
 }
